e11bafdfd310bfe75e5b399f9e2c40fadba3c5b7
isisxxx <iagredot@estudiante.uniajc.edu.co>
2025-09-07 18:21:56 -0500

    correccion estructura menu 1

--- scripts.js
+++ scripts.js
@@ -1,11 +1,2 @@
 // Aquí puedes agregar animaciones o funciones interactivas
 console.log("Critical Hub E-Sports cargado ✅");
-// ===== MENÚ HAMBURGUESA =====
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navMenu = document.querySelector("nav ul");
-
-  menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
-  });
-});
